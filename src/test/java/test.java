4b5bcc78f1e78150bca057070ebc2e31cb64d38f
kexinheart <kexin8816@gmail.com>
2017-09-16 17:50:34 -0400

    command line with  multiple header

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -2,34 +2,42 @@ import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
 import java.io.IOException;
+import java.util.List;
+import java.lang.*;
 
 import static java.util.Arrays.asList;
 class test {
     public static void main(String[] args) throws IOException {
         OptionParser parser = new OptionParser();
 
-        parser.acceptsAll(asList("version", "v"), "protocal version")
+        parser.accepts( "v", "for test")
                 .withOptionalArg();
-        parser.acceptsAll(asList("header", "h"), "request head")
+        parser.accepts("h", "request head")
                 .withOptionalArg()
                 .defaultsTo("localhost");
-        parser.acceptsAll(asList("inline_data", "d"), "inline data")
+        parser.accepts( "d", "inline data")
                 .withOptionalArg();
-        parser.acceptsAll(asList("file", "f"), "source file of request")
+        parser.accepts( "f", "source file of request")
                 .withOptionalArg();
 
 
         OptionSet opts = parser.parse(args);
 
-        String header = (String) opts.valueOf("header");
-        String version = (String) opts.valueOf("version");
-        String inline_data = (String) opts.valueOf("inline_data");
-        String file = (String) opts.valueOf("file");
+        String version = (String) opts.valueOf("v");
+        List<String> header;
+        header = (List<String>) opts.valuesOf("h");
+        String inline_data = (String) opts.valueOf("d");
+        String file = (String) opts.valueOf("f");
+
 
         System.out.println(version);
-        System.out.println(header);
+        for(int i = 0 ; i < header.size() ; i++) {
+            System.out.println(header.get(i));
+        }
         System.out.println(inline_data);
         System.out.println(file);
+
+
     }
 
 
