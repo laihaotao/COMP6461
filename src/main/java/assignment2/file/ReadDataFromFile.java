390a797312cc776ebdd957fe71c6f17e85ff38cd
kexinheart <kexin8816@gmail.com>
2017-10-12 15:05:28 -0400

    add IO to package file

---
+++ src/main/java/assignment2/file/ReadDataFromFile.java
@@ -0,0 +1,49 @@
+package assignment2.file;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+
+import java.io.*;
+
+/**
+ * Created by heart on 2017-10-12.
+ */
+public class ReadDataFromFile {
+    public static String readFile1(String filename) throws IOException {
+        File sourceFile = new File(filename);
+        FileInputStream fileInputStream = new FileInputStream(sourceFile);
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+        BufferedReader br = new BufferedReader(inputStreamReader);
+        StringBuilder content = new StringBuilder();
+        String line = new String();
+        while ((line = br.readLine()) != null) {
+
+            content.append(line+"\n");
+        }
+        br.close();
+        inputStreamReader.close();
+        fileInputStream.close();
+        String fileContent = content.toString();
+        return fileContent;
+    }
+    public static String readFile(String filename) throws IOException {
+        File sourceFile = new File(filename);
+        FileReader fileReader = new FileReader(sourceFile);
+        BufferedReader br = new BufferedReader(fileReader);
+        StringBuilder content = new StringBuilder();
+        String line = new String();
+        while ((line = br.readLine()) != null) {
+
+            content.append(line+"\n");
+        }
+        br.close();
+        fileReader.close();
+        String fileContent = content.toString();
+        return fileContent;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String fileName="outputFile.txt";
+        System.out.println(ReadDataFromFile.readFile1(fileName));
+    }
+}
+
