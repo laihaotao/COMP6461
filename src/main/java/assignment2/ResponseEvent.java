9b93c68a8a8df7db5afde1531a80ca318dd50555
haotao.lai <haotao.lai@gmail.com>
2017-10-21 11:12:38 -0400

    add write data to the client logic

---
+++ src/main/java/assignment2/ResponseEvent.java
@@ -0,0 +1,19 @@
+package assignment2;
+
+import java.nio.channels.SocketChannel;
+
+/**
+ * Author:  Eric(Haotao) Lai
+ * Date:    2017-10-21
+ * E-mail:  haotao.lai@gmail.com
+ * Website: http://laihaotao.me
+ */
+
+
+public class ResponseEvent extends Event{
+
+
+    public ResponseEvent(String rawData, SocketChannel from) {
+        super(rawData, from);
+    }
+}
