974ff64ac323f0498124fa4dff0a94ddc93eec6b
haotao.lai <haotao.lai@gmail.com>
2017-10-21 10:43:37 -0400

    add HttpRequest class

---
+++ src/main/java/assignment2/request/HttpRequest.java
@@ -0,0 +1,14 @@
+package assignment2.request;
+
+/**
+ * Author:  Eric(Haotao) Lai
+ * Date:    2017-10-21
+ * E-mail:  haotao.lai@gmail.com
+ * Website: http://laihaotao.me
+ */
+
+
+public class HttpRequest {
+
+    public String targetPath;
+}
